bdcc39173ec986f4f9acef83da6efe84333133f4
supervova <nickysheen@yandex.ru>
2012-12-04 18:37:27 +0400

    Изменения оформления согласно пожеланиям заказчика
    1) Обновить в скриптах (шаблон scripts.htm) jQuery: версию 1.8.0
    заменить на 1.8.3
    2) Там же удалить jQuery Mobile
    3) Обновить графику — см. коммит
    4) Удалить неиспользуемые скрипты — см. коммит
    5) Добавить в папку скриптов jquery-1.8.3.min.js
    6) В scripts.js заменить функцию slideOut
    7) В «шапках» шаблонов заменить meta-vieport — см. коммит
    8) Там же удалить ссылку на /apple-touch-icon-57x57.png
    9) Там же поменять ссылки на скрипты: modernizr-2.6.1.min.js и
    css3-mediaqueries.js на html5.js и respond.min.js
    10) Обновить styles.css

--- a/js/scripts.js
+++ a/js/scripts.js
@@ -19,7 +19,7 @@
 // SLIDER
 (function($) {
     $.fn.innerfade = function(options) {
-        return this.each(function() {   
+        return this.each(function() {
             $.innerfade(this, options);
         });
     };
@@ -53,9 +53,9 @@
             } else if (settings.type == "random") {
             		var last = Math.floor ( Math.random () * ( elements.length ) );
                 setTimeout(function() {
-                    do { 
+                    do {
 												current = Math.floor ( Math.random ( ) * ( elements.length ) );
-										} while (last == current );             
+										} while (last == current );
 										$.innerfade.next(elements, settings, current, last);
                 }, settings.timeout);
                 $(elements[last]).show();
@@ -112,7 +112,7 @@ function removeFilter(element) {
 }
 
 // PLACEHOLDER polyfill - http://mths.be/placeholder v2.0.7 by @mathias
-;(function(f,h,$){var a='placeholder' in h.createElement('input'),d='placeholder' in h.createElement('textarea'),i=$.fn,c=$.valHooks,k,j;if(a&&d){j=i.placeholder=function(){return this};j.input=j.textarea=true}else{j=i.placeholder=function(){var l=this;l.filter((a?'textarea':':input')+'[placeholder]').not('.placeholder').bind({'focus.placeholder':b,'blur.placeholder':e}).data('placeholder-enabled',true).trigger('blur.placeholder');return l};j.input=a;j.textarea=d;k={get:function(m){var l=$(m);return l.data('placeholder-enabled')&&l.hasClass('placeholder')?'':m.value},set:function(m,n){var l=$(m);if(!l.data('placeholder-enabled')){return m.value=n}if(n==''){m.value=n;if(m!=h.activeElement){e.call(m)}}else{if(l.hasClass('placeholder')){b.call(m,true,n)||(m.value=n)}else{m.value=n}}return l}};a||(c.input=k);d||(c.textarea=k);$(function(){$(h).delegate('form','submit.placeholder',function(){var l=$('.placeholder',this).each(b);setTimeout(function(){l.each(e)},10)})});$(f).bind('beforeunload.placeholder',function(){$('.placeholder').each(function(){this.value=''})})}function g(m){var l={},n=/^jQuery\d+$/;$.each(m.attributes,function(p,o){if(o.specified&&!n.test(o.name)){l[o.name]=o.value}});return l}function b(m,n){var l=this,o=$(l);if(l.value==o.attr('placeholder')&&o.hasClass('placeholder')){if(o.data('placeholder-password')){o=o.hide().next().show().attr('id',o.removeAttr('id').data('placeholder-id'));if(m===true){return o[0].value=n}o.focus()}else{l.value='';o.removeClass('placeholder');l==h.activeElement&&l.select()}}}function e(){var q,l=this,p=$(l),m=p,o=this.id;if(l.value==''){if(l.type=='password'){if(!p.data('placeholder-textinput')){try{q=p.clone().attr({type:'text'})}catch(n){q=$('').attr($.extend(g(this),{type:'text'}))}q.removeAttr('name').data({'placeholder-password':true,'placeholder-id':o}).bind('focus.placeholder',b);p.data({'placeholder-textinput':q,'placeholder-id':o}).before(q)}p=p.removeAttr('id').hide().prev().attr('id',o).show()}p.addClass('placeholder');p[0].value=p.attr('placeholder')}else{p.removeClass('placeholder')}}}(this,document,jQuery)); 
+;(function(f,h,$){var a='placeholder' in h.createElement('input'),d='placeholder' in h.createElement('textarea'),i=$.fn,c=$.valHooks,k,j;if(a&&d){j=i.placeholder=function(){return this};j.input=j.textarea=true}else{j=i.placeholder=function(){var l=this;l.filter((a?'textarea':':input')+'[placeholder]').not('.placeholder').bind({'focus.placeholder':b,'blur.placeholder':e}).data('placeholder-enabled',true).trigger('blur.placeholder');return l};j.input=a;j.textarea=d;k={get:function(m){var l=$(m);return l.data('placeholder-enabled')&&l.hasClass('placeholder')?'':m.value},set:function(m,n){var l=$(m);if(!l.data('placeholder-enabled')){return m.value=n}if(n==''){m.value=n;if(m!=h.activeElement){e.call(m)}}else{if(l.hasClass('placeholder')){b.call(m,true,n)||(m.value=n)}else{m.value=n}}return l}};a||(c.input=k);d||(c.textarea=k);$(function(){$(h).delegate('form','submit.placeholder',function(){var l=$('.placeholder',this).each(b);setTimeout(function(){l.each(e)},10)})});$(f).bind('beforeunload.placeholder',function(){$('.placeholder').each(function(){this.value=''})})}function g(m){var l={},n=/^jQuery\d+$/;$.each(m.attributes,function(p,o){if(o.specified&&!n.test(o.name)){l[o.name]=o.value}});return l}function b(m,n){var l=this,o=$(l);if(l.value==o.attr('placeholder')&&o.hasClass('placeholder')){if(o.data('placeholder-password')){o=o.hide().next().show().attr('id',o.removeAttr('id').data('placeholder-id'));if(m===true){return o[0].value=n}o.focus()}else{l.value='';o.removeClass('placeholder');l==h.activeElement&&l.select()}}}function e(){var q,l=this,p=$(l),m=p,o=this.id;if(l.value==''){if(l.type=='password'){if(!p.data('placeholder-textinput')){try{q=p.clone().attr({type:'text'})}catch(n){q=$('').attr($.extend(g(this),{type:'text'}))}q.removeAttr('name').data({'placeholder-password':true,'placeholder-id':o}).bind('focus.placeholder',b);p.data({'placeholder-textinput':q,'placeholder-id':o}).before(q)}p=p.removeAttr('id').hide().prev().attr('id',o).show()}p.addClass('placeholder');p[0].value=p.attr('placeholder')}else{p.removeClass('placeholder')}}}(this,document,jQuery));
 
 $(document).ready(function(){
 	$(':input[placeholder], textarea[placeholder]').placeholder();
@@ -124,68 +124,56 @@ $(document).ready(function(){
 
 // EQUAL HEIGHTS COLUMNS
 function equalHeights() {
-    //set the starting bigestHeight variable  
+    //set the starting bigestHeight variable
     var biggestHeight = 0;
-    //check each of them  
+    //check each of them
     $('.equal-height .col').each(function () {
-        //if the height of the current element is  
-        //bigger then the current biggestHeight value  
+        //if the height of the current element is
+        //bigger then the current biggestHeight value
         if ($(this).height() > biggestHeight) {
-            //update the biggestHeight with the  
-            //height of the current elements  
+            //update the biggestHeight with the
+            //height of the current elements
             biggestHeight = $(this).height();
         }
     });
-    //when checking for biggestHeight is done set that  
-    //height to all the elements  
+    //when checking for biggestHeight is done set that
+    //height to all the elements
     $('.equal-height .col').height(biggestHeight);
 }
 
-// FACEBOOK-LIKE SLIDE-OUT MENU. Author: Aldo Lugo — http://blog.aldomatic.com/facebook-style-slide-out-menu-in-jquery-mobile/ 
-$(function slideOut(){
-	var menuStatus;
-	
-	$('.btn-menu').click(function(){
-		if(menuStatus != true){				
-		$('.ui-page-active, .doc-header').animate({
-			marginLeft: '240px',
-		  }, 300, function(){menuStatus = true});
-		  return false;
-		  } else {
-			$('.ui-page-active, .doc-header').animate({
-			marginLeft: '0',
-		  }, 300, function(){menuStatus = false});
-			return false;
-		  }
-	});
+// SLIDE-OUT MENU
+$(function slideOut() {
+    var menuStatus;
+    $('.btn-menu').click(function() {
+        if(menuStatus !== true) {
+            $('.page, .doc-header').animate({
+                marginLeft: '240px'
+            }, 300, function() {
+                menuStatus = true;
+            });
+            $('#slide-out').css('display', 'block');
+            return false;
+        } else {
+            $('.page, .doc-header').animate({
+                marginLeft: '0'
+            }, 300, function() {
+                menuStatus = false;
+            });
+            return false;
+        }
+    });
 
-	$('.page').live('swipeleft', function(){
-		if (menuStatus){	
-		$('.ui-page-active, .doc-header').animate({
-			marginLeft: '0',
-		  }, 300, function(){menuStatus = false});
-		  }
-	});
-	
-	$('.page').live('swiperight', function(){
-		if (!menuStatus){	
-		$('.ui-page-active, .doc-header').animate({
-			marginLeft: '240px',
-		  }, 300, function(){menuStatus = true});
-		  }
-	});
-	
-	$('#slide-out li a').click(function(){
-		var p = $(this).parent();
-		if($(p).hasClass('current')){
-			$('#slide-out li').removeClass('current');
-		} else {
-			$('#slide-out li').removeClass('current');
-			$(p).addClass('current');
-		}
-	});
-		
-});	
+    $('#slide-out li a').click(function() {
+        var p = $(this).parent();
+        if($(p).hasClass('current')) {
+            $('#slide-out li').removeClass('current');
+        } else {
+            $('#slide-out li').removeClass('current');
+            $(p).addClass('current');
+        }
+    });
+});
+
 
 function noError(){return true;}
 window.onerror = noError;
